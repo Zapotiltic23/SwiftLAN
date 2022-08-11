098aba539b8b5210c7413c3fc56202eaad7bd928
Horacio Alexandro <horacio@swoopin.net>
2022-08-11 00:10:55 -0700

    Changes

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftLAN",
+            name: "SwiftLAN_Engine",
             targets: ["SwiftLAN_Engine"]),
     ],
     dependencies: [
