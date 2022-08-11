b5f9cf03d4a303d8f2205d9427922184a2ea705b
Horacio Alexandro <horacio@swoopin.net>
2022-08-10 23:57:29 -0700

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLAN",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftLAN",
-            targets: ["SwiftLAN"]),
+            targets: ["SwiftLAN_Engine"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SwiftLAN",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftLANTests",
-            dependencies: ["SwiftLAN"]),
+        .binaryTarget(
+            name: "SwiftLAN_Engine",
+            path: "./Sources/SwiftLAN_Engine.xcframework")
     ]
 )
---
+++ Sources/SwiftLAN_Engine.xcframework/ios-arm64_x86_64-simulator/SwiftLAN_Engine.framework/Headers/SwiftLAN_Engine.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLAN_Engine.h
+//  SwiftLAN_Engine
+//
+//  Created by Horacio Alexandro Sanchez on 8/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLAN_Engine.
+FOUNDATION_EXPORT double SwiftLAN_EngineVersionNumber;
+
+//! Project version string for SwiftLAN_Engine.
+FOUNDATION_EXPORT const unsigned char SwiftLAN_EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLAN_Engine/PublicHeader.h>
+
+
